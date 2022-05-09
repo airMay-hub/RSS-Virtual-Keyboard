72178eaccd57e30e1209ffa90cc57e15c3ad6494
Maryia Tomkiavichutse <kiiikaaa.93@mail.ru>
2022-05-09 22:32:08 +0300

    feat: make functions that print keys not to print unnecessary keys, like 'Backspace'

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
-// Add layout ====================================
 import './sass/style.scss';
 import pasteLayout from './layout';
 
 pasteLayout();
 
-// Press keys on keyboard =========================
-const TEXTAREA = document.querySelector('.output');
-
-// Animation when press ================================
 const keys = document.querySelectorAll('.key');
 
+// Animation when press ================================
 window.addEventListener('keydown', (e) => {
   for (let i = 0; i < keys.length; i += 1) {
     if (keys[i].classList.contains(e.code)) {
       keys[i].classList.add('press');
-      TEXTAREA.innerHTML += e.key;
     }
   }
 });
@@ -27,9 +22,69 @@ window.addEventListener('keyup', (e) => {
   }
 });
 
-// const caseUp = document.querySelectorAll('.caseUp');
-// const caps = document.querySelectorAll('.caps');
-// const shiftCaps = document.querySelectorAll('.shiftCaps');
+// Animation when click ===============================
+keys.forEach((item) => {
+  item.addEventListener('mousedown', (e) => {
+    e.currentTarget.classList.add('press');
+  });
+});
+
+keys.forEach((item) => {
+  item.addEventListener('mouseup', (e) => {
+    e.currentTarget.classList.remove('press');
+  });
+  item.addEventListener('mouseleave', (e) => {
+    e.currentTarget.classList.remove('press');
+  });
+});
+
+// Fill textarea by keys =============================
+const TEXTAREA = document.querySelector('.output');
+
+const arrOfNonePrintKeys = ['Backspace', 'Tab', 'Delete', 'Del', 'CapsLock', 'Enter', 'Shift', 'ShiftLeft', 'ShiftRight', 'ControlLeft', 'Ctrl', 'MetaLeft', 'Win', 'Alt', 'AltLeft', 'AltRight', 'ControlRight'];
+
+const arrow = {
+  ArrowUp: '&uarr;',
+  ArrowLeft: '&larr;',
+  ArrowDown: '&darr;',
+  ArrowRight: '&rarr;',
+};
+
+window.addEventListener('keydown', (e) => {
+  if (e.key.startsWith('Arrow')) {
+    TEXTAREA.innerHTML += arrow[e.key];
+  } else {
+    for (let i = 0; i < keys.length; i += 1) {
+      if (keys[i].classList.contains(e.code)
+      && !arrOfNonePrintKeys.includes(e.code)) {
+        TEXTAREA.innerHTML += e.key;
+      }
+    }
+  }
+});
+
+// Fill textarea by clicks =============================
+function printLetter(collection, event) {
+  let letter = '';
+  for (let i = 0; i < collection.length; i += 1) {
+    if (!collection[i].classList.contains('hidden')) {
+      const targetLetter = Array.from(event.currentTarget.children[i].children)
+        .find((a) => !a.classList.contains('hidden'));
+      if (!arrOfNonePrintKeys.includes(targetLetter.innerHTML)) {
+        letter = targetLetter.innerHTML;
+      } else {
+        letter = '';
+      }
+    }
+  }
+  return letter;
+}
+
+keys.forEach((item) => {
+  item.addEventListener('mousedown', (e) => {
+    TEXTAREA.innerHTML += printLetter(e.currentTarget.children, e);
+  });
+});
 
 // Change language ====================================
 const RUS = document.querySelectorAll('.key__rus');
@@ -63,13 +118,6 @@ window.addEventListener('keyup', (e) => {
   }
 });
 
-// Clicks on keys ==========================
-const KEYBOARD = document.querySelector('.keyboard');
-
-KEYBOARD.addEventListener('mousedown', (e) => {
-  e.target.classList.add('press');
-});
-
-KEYBOARD.addEventListener('mouseup', (e) => {
-  e.target.classList.remove('press');
-});
+// const caseUp = document.querySelectorAll('.caseUp');
+// const caps = document.querySelectorAll('.caps');
+// const shiftCaps = document.querySelectorAll('.shiftCaps');
